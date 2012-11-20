25bb8baa5fb6e930467e2310e323b040c8afe384
Dmytro Vorona <alendit@gmail.com>
2012-11-20 14:37:48 +0100

    Missing semicolon added

--- lang/en/filter_mathjax.php
+++ lang/en/filter_mathjax.php
@@ -6,5 +6,5 @@ $string['distroothttp'] = 'MathJax base URL (http)';
 $string['distroothttp_descr'] = 'HTTP URL to a preferred MathJax distribution. When left empty, the MathJax.org CDN will be used.';
 $string['distroothttps'] = 'MathJax base URL (https)';
 $string['distroothttps_descr'] = 'HTTPS URL to a preferred MathJax distribution. When left empty, the MathJax.org CDN will be used.';
-$string['singledollar'] = 'Parse single-dollar'
-$string['singledollar_descr'] = 'Accept inline math in  single-dollar delimiters'
+$string['singledollar'] = 'Parse single-dollar';
+$string['singledollar_descr'] = 'Accept inline math in  single-dollar delimiters';
