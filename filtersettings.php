d56f02ab7157e7a879b3c94e07b32d42907182f8
Dmytro Vorona <alendit@gmail.com>
2012-11-20 14:19:06 +0100

    Dummy settings added

--- filtersettings.php
+++ filtersettings.php
@@ -10,4 +10,8 @@ if ($ADMIN->fulltree) {
     $settings->add(new admin_setting_configtext('filter_mathjax_distroothttps',
                        get_string('distroothttps', 'filter_mathjax'),
                        get_string('distroothttps_descr', 'filter_mathjax'), ''));
+    $settings->add(new admin_setting_configcheckbox('filter_mathjax_singledollar',
+                       get_string('singledollar', 'filter_mathjax'),
+                       get_string('singledollar_descr', 'filter_mathjax'), ''));
+
 }
