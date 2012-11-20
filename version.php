18142dc2b30a89d0b163d76ca0633f5babca6d63
Dmytro Vorona <alendit@gmail.com>
2012-11-20 16:15:07 +0100

    Version bump

--- version.php
+++ version.php
@@ -2,6 +2,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2012110700;
+$plugin->version   = 2012112000;
 $plugin->requires  = 2011112900;        // Requires this Moodle version
 $plugin->component = 'filter_mathjax';  // Full name of the plugin (used for diagnostics)
